0c7131a8dc023f96e9aed513f5f7eae14952fb47
Ravindra <sirviravindra609@gmail.com>
2023-09-19 23:14:49 +0530

    login Component Changes

--- ngrx-demo/src/app/core/login/login.component.ts
+++ ngrx-demo/src/app/core/login/login.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-login",
@@ -6,5 +8,11 @@ import { Component } from "@angular/core";
   styles: [],
 })
 export class LoginComponent {
-  
+  private baseUrl = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/users`);
+  }
 }
